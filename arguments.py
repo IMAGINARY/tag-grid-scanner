30329dcf335cdecb64cfa83ba135e80307905a51
Christian Stussak <stussak@mfo.de>
2021-09-10 15:58:02 +0200

    Add argument and config file parser and config validator

---
+++ arguments.py
@@ -0,0 +1,19 @@
+import argparse
+import pathlib
+
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument(
+        "CONFIG_FILE",
+        type=pathlib.Path,
+        nargs=1,
+        help="path to the JSON configuration file",
+    )
+    return parser
+
+
+def get_arguments():
+    parser = get_argument_parser()
+    args = vars(parser.parse_args())
+    return args
