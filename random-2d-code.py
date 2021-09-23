78a9ad1b3a4690b29d23c6560301ebac82c6814d
Christian Stussak <stussak@mfo.de>
2021-09-23 12:48:35 +0200

    Remove random-2d-code.py
    This programm is superseeded by the 'display' command.

--- random-2d-code.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-GRID_SHAPE = (16 * 4, 16 * 4)
-
-img = np.random.randint(2, size=GRID_SHAPE, dtype=np.uint8) * 255
-
-cv2.imwrite("code_{}x{}.png".format(GRID_SHAPE[0], GRID_SHAPE[1]), img)
