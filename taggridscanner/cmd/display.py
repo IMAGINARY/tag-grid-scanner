331cb8bd369189c92c9e578f9fa69c67a7702ec3
Christian Stussak <stussak@mfo.de>
2021-10-15 21:07:54 +0200

    Refactor display command towards new config format

--- taggridscanner/cmd/display.py
+++ taggridscanner/cmd/display.py
@@ -4,25 +4,19 @@ import numpy as np
 import random
 
 from taggridscanner.pipeline.detect_tags import string_tag_to_np_tag
-from taggridscanner.aux.utils import (
-    compute_rel_gap,
-    compute_rel_margin_trbl,
-)
 from taggridscanner.pipeline.preprocess import (
     create_linear_transformer,
     create_inverse_linear_transformer,
 )
 
-
 GAP_COLOR = 256 / 2
-FRAME_COLOR = 3 * 256 / 4
-BORDER_COLOR = 0
+BG_COLOR = 256 / 4
 
 
 def rotate_tag(tag, rotation=0):
     while rotation > 0:
         tag = np.rot90(tag)
-        rotation = rotation - 1
+        rotation -= 1
     return tag
 
 
@@ -52,12 +46,29 @@ def draw_pattern(img, np_tags, grid_shape, rel_gap):
             draw_tile(tile_img, tag)
 
 
-def draw_roi(img, np_tags, grid_shape, rel_margin_trbl, rel_gap):
-    y_start = math.floor(img.shape[0] * rel_margin_trbl[0])
-    y_end = math.ceil(img.shape[0] * (1.0 - rel_margin_trbl[2]))
+def draw_roi(
+    img,
+    np_tags,
+    grid_shape,
+    rel_gap,
+):
+    img_aspect_ratio = img.shape[1] / img.shape[0]
 
-    x_start = math.floor(img.shape[1] * rel_margin_trbl[3])
-    x_end = math.ceil(img.shape[1] * (1.0 - rel_margin_trbl[1]))
+    roi_width_with_gaps = grid_shape[1] + (grid_shape[1] - 1) * rel_gap[1]
+    roi_height_with_gaps = grid_shape[0] + (grid_shape[0] - 1) * rel_gap[0]
+    roi_aspect_ratio = roi_width_with_gaps / roi_height_with_gaps
+
+    roi_size = (
+        (img.shape[0], img.shape[0] * roi_aspect_ratio)
+        if img_aspect_ratio > roi_aspect_ratio
+        else (img.shape[1] / roi_aspect_ratio, img.shape[1])
+    )
+
+    y_start = math.floor(img.shape[0] / 2 - roi_size[0] / 2)
+    y_end = math.ceil(img.shape[0] / 2 + roi_size[0] / 2)
+
+    x_start = math.floor(img.shape[1] / 2 - roi_size[1] / 2)
+    x_end = math.ceil(img.shape[1] / 2 + roi_size[1] / 2)
 
     roi_img = img[y_start:y_end, x_start:x_end]
     roi_img.fill(GAP_COLOR)
@@ -65,53 +76,12 @@ def draw_roi(img, np_tags, grid_shape, rel_margin_trbl, rel_gap):
     draw_pattern(roi_img, np_tags, grid_shape, rel_gap)
 
 
-def draw_frame(
-    img,
-    np_tags,
-    grid_shape,
-    abs_frame_size,
-    abs_margin_trbl,
-    abs_gap,
-):
-    img_aspect_ratio = img.shape[1] / img.shape[0]
-    frame_aspect_ratio = abs_frame_size[1] / abs_frame_size[0]
-
-    frame_size = (
-        (img.shape[0], img.shape[0] * frame_aspect_ratio)
-        if img_aspect_ratio > frame_aspect_ratio
-        else (img.shape[1] / frame_aspect_ratio, img.shape[1])
-    )
-
-    y_start = math.floor(img.shape[0] / 2 - frame_size[0] / 2)
-    y_end = math.ceil(img.shape[0] / 2 + frame_size[0] / 2)
-
-    x_start = math.floor(img.shape[1] / 2 - frame_size[1] / 2)
-    x_end = math.ceil(img.shape[1] / 2 + frame_size[1] / 2)
-
-    frame_img = img[y_start:y_end, x_start:x_end]
-    frame_img.fill(FRAME_COLOR)
-
-    rel_margin_trbl = compute_rel_margin_trbl(abs_frame_size, abs_margin_trbl)
-
-    rel_gap = compute_rel_gap(abs_frame_size, abs_margin_trbl, abs_gap)
-
-    draw_roi(
-        frame_img,
-        np_tags,
-        grid_shape,
-        rel_margin_trbl,
-        rel_gap,
-    )
-
-
 def create_img(
     img_size,
     rel_border_size,
     np_tags,
     grid_shape,
-    abs_frame_size,
-    abs_margin_trbl,
-    abs_gap_vh,
+    rel_gap,
     rotate,
     flip_h,
     flip_v,
@@ -119,7 +89,7 @@ def create_img(
     linear_transform = create_linear_transformer(rotate, flip_h, flip_v)
     inverse_linear_transform = create_inverse_linear_transformer(rotate, flip_h, flip_v)
 
-    img = np.full(img_size, BORDER_COLOR, np.uint8)
+    img = np.full(img_size, BG_COLOR, np.uint8)
     img = inverse_linear_transform(img)
 
     y_start = math.floor(img.shape[0] * rel_border_size[0])
@@ -129,14 +99,7 @@ def create_img(
     x_end = math.ceil(img.shape[1] * (1.0 - rel_border_size[1]))
 
     img_without_border = img[y_start:y_end, x_start:x_end]
-    draw_frame(
-        img_without_border,
-        np_tags,
-        grid_shape,
-        abs_frame_size,
-        abs_margin_trbl,
-        abs_gap_vh,
-    )
+    draw_roi(img_without_border, np_tags, grid_shape, rel_gap)
 
     return linear_transform(img)
 
@@ -151,10 +114,7 @@ def display(args):
 
     tag_shape = tuple(config_with_defaults["dimensions"]["tile"])
     grid_shape = tuple(config_with_defaults["dimensions"]["grid"])
-
-    abs_frame_size = tuple(config_with_defaults["dimensions"]["size"])
-    abs_margin_trbl = tuple(config_with_defaults["dimensions"]["padding"])
-    abs_gap = tuple(config_with_defaults["dimensions"]["gap"])
+    rel_gap = tuple(config_with_defaults["dimensions"]["gap"])
 
     np_tags = [
         string_tag_to_np_tag(t, tag_shape) for t in config["tags"] if t != "unknown"
@@ -168,9 +128,7 @@ def display(args):
             rel_border_size,
             np_tags,
             grid_shape,
-            abs_frame_size,
-            abs_margin_trbl,
-            abs_gap,
+            rel_gap,
             rotate,
             flip_h,
             flip_v,
