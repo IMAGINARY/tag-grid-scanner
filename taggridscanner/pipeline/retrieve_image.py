b16dd1d82440fbe734501e6e44bc93e0ea82b579
Christian Stussak <stussak@mfo.de>
2021-10-20 13:36:22 +0200

    Add automatic reconnection and unify image source
    Note that the automatic reconnection might not work as expected if there
    are muptiple cameras connected to the system. OpenCV addresses cameras
    by their index and certain backends change the indices of already
    connected cameras when a another camera is connected or disconnected.
    One notable example of such a backend is AVFoundation on macOS.

--- taggridscanner/pipeline/retrieve_image.py
+++ taggridscanner/pipeline/retrieve_image.py
@@ -1,28 +1,89 @@
-from abc import ABCMeta, abstractmethod
+import threading
 import time
 import cv2
-import numpy as np
 
-from taggridscanner.aux.threading import WorkerThread, SynchronizedObjectProxy
+from taggridscanner.aux.threading import WorkerThread
 
 
-class RetrieveImage(metaclass=ABCMeta):
-    def __init__(self, capture):
+class RetrieveImage:
+    def __init__(
+        self,
+        id_or_filename,
+        api_preference=cv2.CAP_ANY,
+        props=None,
+        reconnection_delay=0.5,
+    ):
         super().__init__()
-        self.capture = capture
+        if props is None:
+            props = []
+        self.id_or_filename = id_or_filename
+        self.api_preference = api_preference
+        self.reconnection_delay = reconnection_delay
+        self.props = props
+        self.capture = cv2.VideoCapture()
+        self.__last_reconnection_ts = float("-inf")
+        self.__rlock = threading.RLock()
+        self.__last_image = self.__read_and_block_when_disconnected()
+        self.__worker = WorkerThread(lambda: self.__read())
+        self.__worker.start()
 
-    @abstractmethod
+    @property
+    def rlock(self):
+        return self.__rlock
+
     def read(self):
-        pass
+        fps = self.capture.get(cv2.CAP_PROP_FPS)
+        self.__worker.rate_limit = 60.0 if fps == 0.0 else fps
+        return self.__worker.result.retrieve()
+
+    def __read_and_block_when_disconnected(self):
+        with self.rlock:
+            ret, image = self.capture.read()
+            while not ret:
+                self.reconnect()
+                ret, image = self.capture.read()
+            return image
+
+    def __read(self):
+        with self.rlock:
+            ret, image = self.capture.read()
+            if not ret:
+                self.reconnect()
+                ret, image = self.capture.read()
+
+            if ret:
+                self.__last_image = image
+            return self.__last_image
+
+    def reconnect(self):
+        with self.rlock:
+            ret = False
+            while not ret:
+                reconnection_ts = time.perf_counter()
+                reconnection_delay = self.reconnection_delay - (
+                    reconnection_ts - self.__last_reconnection_ts
+                )
+                self.__last_reconnection_ts = reconnection_ts
+                if reconnection_delay > 0:
+                    time.sleep(reconnection_delay)
+
+                self.capture.release()
+                ret = self.capture.open(self.id_or_filename, self.api_preference)
+                if ret:
+                    for (prop_id, prop_value) in self.props:
+                        self.capture.set(prop_id, prop_value)
 
     @property
     def size(self):
-        w = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        h = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        return (h, w)
+        with self.rlock:
+            w = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+            h = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            return (h, w)
 
     def __del(self):
-        self.capture.release()
+        with self.rlock:
+            self.__worker.stop()
+            self.capture.release()
 
     def __call__(self):
         return self.read()
@@ -32,93 +93,24 @@ class RetrieveImage(metaclass=ABCMeta):
         camera_config = config["camera"]
         use_camera_device = "id" in camera_config
         source = camera_config["id"] if use_camera_device else camera_config["filename"]
-        capture = cv2.VideoCapture(source)
 
+        props = []
         if "fourcc" in camera_config:
             s = camera_config["fourcc"]
             fourcc = cv2.VideoWriter_fourcc(s[0], s[1], s[2], s[3])
-            capture.set(cv2.CAP_PROP_FOURCC, fourcc)
+            props.append((cv2.CAP_PROP_FOURCC, fourcc))
 
         if "size" in camera_config:
             [height, width] = camera_config["size"]
-            capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-            capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+            props.append((cv2.CAP_PROP_FRAME_WIDTH, width))
+            props.append((cv2.CAP_PROP_FRAME_HEIGHT, height))
 
         if "fps" in camera_config:
             fps = camera_config["fps"]
-            capture.set(cv2.CAP_PROP_FPS, fps)
+            props.append((cv2.CAP_PROP_FPS, fps))
 
         if "exposure" in camera_config:
             exposure = camera_config["exposure"]
-            capture.set(cv2.CAP_PROP_EXPOSURE, exposure)
+            props.append((cv2.CAP_PROP_EXPOSURE, exposure))
 
-        if use_camera_device:
-            return RetrieveFromCamera(capture)
-        else:
-            num_frames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
-            if num_frames == 1.0:
-                # This is just a heuristic. OpenCV's capture API sucks.
-                return RetrieveFromSingleImage(capture)
-            else:
-                return RetrieveFromVideo(capture)
-
-
-class RetrieveFromCamera(RetrieveImage):
-    def __init__(self, capture):
-        super().__init__(capture)
-
-    def read(self):
-        ret, img = self.capture.read()
-        return img if ret else np.zeros(self.size, np.uint8)
-
-
-class RetrieveFromSingleImage(RetrieveImage):
-    def __init__(self, capture):
-        super().__init__(capture)
-        fps = 60.0
-        self.capture.set(cv2.CAP_PROP_FPS, fps)
-        self.__last_read_ts = -1.0 / fps
-        ret, img = self.capture.read()
-        self.__image = img if ret else np.zeros(self.size, np.uint8)
-
-    def read(self):
-        ts = time.perf_counter()
-        fps = self.capture.get(cv2.CAP_PROP_FPS)
-        if fps > 0:
-            sleep_time = 1.0 / fps - (ts - self.__last_read_ts)
-            self.__last_read_ts = ts
-            if sleep_time > 0:
-                time.sleep(sleep_time)
-        return self.__image.copy()
-
-
-class RetrieveFromVideo(RetrieveImage):
-    def __init__(self, capture):
-        super().__init__(SynchronizedObjectProxy(capture))
-
-        def read():
-            ret, img = capture.read()
-            if not ret:
-                # reach end of stream -> rewind
-                # (can also happen when there is an input error,
-                # but there is no way in OpenCV to tell the difference)
-                # maybe switch to PyAV for capturing
-                capture.set(cv2.CAP_PROP_POS_FRAMES, 0.0)
-                capture.set(cv2.CAP_PROP_POS_MSEC, 0.0)
-
-                # try again
-                ret, img = self.capture.read()
-                return img if ret else np.zeros(self.size, np.uint8)
-            else:
-                return img
-
-        fps = capture.get(cv2.CAP_PROP_FPS)
-        rate_limit = fps if fps > 0.0 else None
-        self.worker = WorkerThread(read, rate_limit=rate_limit)
-        self.worker.start()
-
-    def __del(self):
-        self.worker.stop()
-
-    def read(self):
-        return self.worker.result.retrieve()
+        return RetrieveImage(source, props=props)
