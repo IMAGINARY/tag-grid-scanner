b9aac605b129b45f5f1f62e41f31a66499283b00
Christian Stussak <stussak@mfo.de>
2021-10-21 09:43:37 +0200

    Add resizing support to ViewImage

--- taggridscanner/pipeline/view_image.py
+++ taggridscanner/pipeline/view_image.py
@@ -7,8 +7,12 @@ class ViewImage(Functor):
     def __init__(self, title=None):
         super().__init__()
         self.__window_name = str(uuid.uuid4())
+        self.__window_flags = (
+            cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_NORMAL
+        )
         self.__title = ""
         self.title = self.__window_name if title is None else title
+        self.__last_image_shape = (0, 0)
 
     @property
     def window_name(self):
@@ -21,17 +25,20 @@ class ViewImage(Functor):
     @title.setter
     def title(self, title):
         self.__title = title
-        cv2.namedWindow(self.__window_name)
+        cv2.namedWindow(self.__window_name, self.__window_flags)
         cv2.setWindowTitle(self.__window_name, title)
 
     def hide(self):
         cv2.destroyWindow(self.__window_name)
 
-    def __call__(self, value):
-        cv2.namedWindow(self.__window_name)
+    def __call__(self, image):
+        cv2.namedWindow(self.__window_name, self.__window_flags)
         cv2.setWindowTitle(self.__window_name, self.__title)
-        cv2.imshow(self.__window_name, value)
-        return value
+        if self.__last_image_shape != image.shape[0:2]:
+            cv2.resizeWindow(self.__window_name, image.shape[1], image.shape[0])
+            self.__last_image_shape = image.shape[0:2]
+        cv2.imshow(self.__window_name, image)
+        return image
 
     def __del__(self):
         self.hide()
