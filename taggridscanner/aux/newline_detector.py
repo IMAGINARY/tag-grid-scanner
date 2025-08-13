23270f1a4580ceefd605153b6c16956a99803ff5
Christian Stussak <christian.stussak@imaginary.org>
2025-08-13 16:08:10 +0200

    Catch `EOFError` in `NewlineDetector` and block indefinitely

--- taggridscanner/aux/newline_detector.py
+++ taggridscanner/aux/newline_detector.py
@@ -1,7 +1,14 @@
-import sys
+import threading
 from taggridscanner.aux.threading import WorkerThreadWithResult
+
+
+def input_catch_eof():
+    try:
+        return input()
+    except EOFError:
+        threading.Event().wait()
 
 
 class NewlineDetector(WorkerThreadWithResult):
     def __init__(self):
-        super().__init__(input)
+        super().__init__(input_catch_eof)
